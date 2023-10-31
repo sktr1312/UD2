de9ace30d6f1eaa68a8bec3b721c52fe95ce84b9
sktr1312 <gutierrezalfonso30@gmail.com>
2023-10-31 12:27:52 +0100

    main2

---
+++ funciones/E0410.java
@@ -0,0 +1,8 @@
+package funciones;
+
+public class E0410 {
+    public static void main(String[] args) {
+        // Diseñar una función que calcule an, donde a es real y n es entero no
+        // negativo. Realizar una versión iterativa y otra recursiva.
+    }
+}
